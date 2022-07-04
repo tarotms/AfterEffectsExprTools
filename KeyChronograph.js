e39a96cddb9220ed327456bdbdb2194a589e6a85
Tarotms <77107642+tarotms@users.noreply.github.com>
2022-07-04 13:22:42 +0800

    Create KeyChronograph.js

---
+++ KeyChronograph.js
@@ -0,0 +1,15 @@
+/*
+Set the timer to 0 at each key
+Return number
+Example:
+KEY   :    o             o     o         o
+RETURN:0-1-0-1-2-3-4-5-6-0-1-2-0-1-2-3-4-0-1-2
+*/
+function KeyChronograph(){
+	_property = thisProperty
+	if(_property.numKeys != 0){
+		if(time<_property.key(1).time){
+		return time}else{n = _property.nearestKey(time).index
+		time>=_property.key(n).time?n:--n
+		return time-_property.key(n).time}}else{return time}}
+KeyChronograph()
